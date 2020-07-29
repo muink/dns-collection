1c76a2d935512f200c0b53bcea2d446b236b23f3
muink <hukk1996@gmail.com>
2020-07-29 17:14:11 +0800

    fix

--- gen_local.sh
+++ gen_local.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 inport='./localDNS.md'
-outport='./localdns.yaml'
+outport='./localdns.yml'
 
 cat << EOF > "$outport"
 # ipcidr
-# Source: https://github.com/muink/dns-collection/blob/master/LocalDNS.yaml
+# Source: https://github.com/muink/dns-collection/blob/master/LocalDNS.yml
 # Last Modified: `date -u '+%F %T %Z'`
 payload:
 EOF
 
-sed -En "3,$ p" "$inport" | cut -f3,4 -d'|' | sed -En "s|\s||g; s/\|/\n/g p" | grep -E [^[:space:]] | sed -En "s|^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)(:[0-9]+)?$|\1/32|; s|^(\[.+\])(:[0-9]+)?$|\1/128|; p" \
+sed -En "3,$ p" "$inport" | cut -f3,4 -d'|' | sed -En "s|\s||g; s/\|/\n/g p" | grep -E [^[:space:]] | sed -En "s|^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)(:[0-9]+)?$|\1/32|; s|^\[(.+)\](:[0-9]+)?$|\1/128|; p" \
 | sed -E "s|^|  - '|; s|$|'|" >> "$outport"
 
 echo -e "\n" >> "$outport"
