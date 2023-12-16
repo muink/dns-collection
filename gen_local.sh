053e43ae7bda5f3379b1f2034522f860ddfb5e7d
Anya Lin <hukk1996@gmail.com>
2023-12-16 13:53:23 +0800

    update

--- gen_local.sh
+++ gen_local.sh
@@ -4,16 +4,41 @@ CURRENTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $CURRENTDIR
 
 inport='LocalDNS.md'
-outport='LocalDNS.yml'
+tmpout='LocalDNS.tmp'
 
-cat << EOF > "$outport"
+sed -En "3,$ p" "$inport" | cut -f3,4 -d'|' | sed -En "s|\s||g; s/\|/\n/g p" | grep -E [^[:space:]] | sed -En "s|^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)(:[0-9]+)?$|\1/32|; s|^\[(.+)\](:[0-9]+)?$|\1/128|; p" > "$tmpout"
+
+clashout='LocalDNS.yml'
+singout='LocalDNS.json'
+
+# clash
+cat << EOF > "$clashout"
 # ipcidr
 # Source: https://github.com/muink/dns-collection/blob/master/LocalDNS.yml
 # Last Modified: `date -u '+%F %T %Z'`
 payload:
 EOF
+cat "$tmpout" | sed -E "s|^|  - '|; s|$|'|" >> "$clashout"
+echo >> "$clashout"
 
-sed -En "3,$ p" "$inport" | cut -f3,4 -d'|' | sed -En "s|\s||g; s/\|/\n/g p" | grep -E [^[:space:]] | sed -En "s|^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)(:[0-9]+)?$|\1/32|; s|^\[(.+)\](:[0-9]+)?$|\1/128|; p" \
-| sed -E "s|^|  - '|; s|$|'|" >> "$outport"
+# sing-box
+cat <<-EOF > "$singout"
+	{
+	  "__Source__": "https://github.com/muink/dns-collection/blob/master/LocalDNS.json",
+	  "__last_modified__": "$(date -u '+%F %T %Z')",
+	  "version": 1,
+	  "rules": [
+	    {
+	      "ip_cidr": [
+EOF
+sed -En 's|^|        "|; s|$|",|; p' "$tmpout" >> "$singout"
+sed -i '${s|,$||}' "$singout"
+cat <<-EOF >> "$singout"
+	      ]
+	    }
+	  ]
+	}
+EOF
 
-echo >> "$outport"
+# cleanup
+rm -f "$tmpout"
